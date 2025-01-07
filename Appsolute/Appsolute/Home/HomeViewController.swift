6bed3a6d79da5f0c981210d8f397cfafadbbaffd
gomminjae <gommj0611@naver.com>
2025-01-07 16:01:23 +0900

    Initial Commit

---
+++ Appsolute/Appsolute/Home/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  Appsolute
+//
+//  Created by 권민재 on 1/7/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .backgroundColor
+
+    
+    }
+    
+
+   
+
+}
---
+++ Appsolute/Appsolute/UIColor+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extension.swift
+//  Appsolute
+//
+//  Created by 권민재 on 1/7/25.
+//
+
+import UIKit
+
+extension UIColor {
+    static var backgroundColor = UIColor(red: 0.83, green: 0.90, blue: 0.98, alpha: 1.00)
+}
