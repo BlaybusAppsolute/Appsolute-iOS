679d6ce43aed6170bd60034325532177f0107d16
gomminjae <gommj0611@naver.com>
2025-01-09 02:36:41 +0900

    Feat: Component

---
+++ Appsolute/Appsolute/Component/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  Untitled.swift
+//  Appsolute
+//
+//  Created by 권민재 on 1/8/25.
+//
+import UIKit
+
+class CalendarView: UIView {
+   
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
